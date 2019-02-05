8fc8780b1d1c3ca5e2d16002cef5709015e97930
maryakshaya <47344083+maryakshaya@users.noreply.github.com>
2019-02-05 12:17:44 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,7 @@
+n = '???'
+n = n.lower()
+letter =['a','e','i','o','u','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+if n in letter:
+	print("Yes")
+else :
+     print("No")
